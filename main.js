1f8dcdf7ec90c9ab35915ab8023cd5599292ea36
Daniel Ruiz <danielruimar@gmail.com>
2023-01-28 13:25:27 +0100

    update

--- main.js
+++ main.js
@@ -20,15 +20,15 @@ let btnVariable;
 
 function pintarBtnBasicos() {
     
-    let btnNewJoke = document.createElement('container__btn');
-    let btnReset = document.createElement('container__btn');
+    let btnNewJoke = document.createElement('button');
+    let btnReset = document.createElement('button');
 
     btnNewJoke.classList.add('container__btn-send','container__btn');
-    btnNewJoke.textContent = 'New Joke';
+    btnNewJoke.textContent = 'NEW JOKE';
     btnDivBox.appendChild(btnNewJoke);
 
     btnReset.classList.add('container__btn-reset','container__btn');
-    btnReset.textContent = 'Reset';
+    btnReset.textContent = 'RESET';
     btnDivBox.appendChild(btnReset);
 
     btnReset.addEventListener('click', borrarCategories);
@@ -45,37 +45,50 @@ function btnCategory() {
 
 function pintarBtn(array) {   
 
-    array.forEach((element) => {
+    array.forEach((element, index) => {
 
         btnVariable = document.createElement('button');
         btnVariable.className = 'container__btn';
+        btnVariable.setAttribute('id',index);
         btnVariable.textContent = element;
         container.appendChild(btnVariable);
-        btnVariable.addEventListener('click',function(){anadirCategoria(element)});
+        btnVariable.addEventListener('click',function(){anadirCategoria(element, index)});
 
     });
 }
 
-function anadirCategoria(categorie) {
-    btnVariable.classList.add('container__btn--select');
-    console.log(btnVariable);
-    categoriesSelected.push(categorie);
+function anadirCategoria(categorie, index) {
+    let btnSelect = document.getElementById(index);
+    btnSelect.classList.add('container__btn--select');
     console.log(categoriesSelected);
-    urlCategorie = urlBasic + categoriesSelected.toString();
-    console.log(urlCategorie);
+    
+        if ( categoriesSelected.includes(categorie)){
+            
+            btnSelect.classList.remove('container__btn--select');
+            let posArrayCtegr = categoriesSelected.indexOf(categorie);
+            categoriesSelected.splice(posArrayCtegr,1);
+            console.log(categoriesSelected);
+        
+    
+        }else{ 
+            categoriesSelected.push(categorie);
+            console.log(categoriesSelected);
+            urlCategorie = urlBasic + categoriesSelected.toString();
+            console.log(urlCategorie);
+        }
 }
 
 function enviarCategories() {
-    if (categoriesSelected === []) {
-        jokeAleatorie();
-    } else {
-        fetch(urlCategorie)
+    let url1 = urlCategorie ;
+    console.log(url1);
+    
+        fetch(url1)
         .then(respuesta => respuesta.json())
         .then(respuesta => joke.textContent = respuesta.value)
         .catch(error => console.log(error))
     }
    
-}
+
 
 function borrarCategories() {
     categoriesSelected = [];
@@ -85,7 +98,10 @@ function borrarCategories() {
 function jokeAleatorie() {
     fetch(urlAleatorie)
     .then(respuesta => respuesta.json())
-    .then(respuesta => joke.textContent = respuesta.value)
+    .then(respuesta => {
+        console.log(respuesta);
+        joke.textContent = respuesta.value;
+    })
     .catch(error => console.log(error)) 
 }
 
